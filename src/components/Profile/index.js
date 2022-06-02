4d53f486115b1c6ac32b43ab19bb70be01e7fb55
Hannah Callison <hannahcallison1007@gmail.com>
2022-06-02 10:27:59 -0700

    char/profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -8,6 +8,36 @@ function Profile() {
       <div className="page">
 
          <h1>Profile</h1>
+
+         <div className="charContainer">
+            {/* Display char info */}
+         </div>
+
+         <form method="post">
+            <div class="container">
+               <label htmlFor="charName"><b>Character Name</b></label>
+               <input type="text" placeholder="Character Name" name="charName" required />
+
+               <button>Auto</button>
+
+               <label htmlFor="class"><b>Class</b></label>
+               <input type="text" placeholder="Enter Class" name="class" required />
+
+               <label htmlFor="itemLevel"><b>Item Level</b></label>
+               <input type="number" placeholder="Enter Item Level" name="itemLevel" required />
+
+               <label htmlFor="rosterLevel"><b>Roster Level</b></label>
+               <input type="number" placeholder="Enter Roster Level" name="rosterLevel" required />
+
+               <label htmlFor="charLevel"><b>Character Level</b></label>
+               <input type="number" placeholder="Enter Character Level" name="charLevel" required />
+
+               <label htmlFor="engravings"><b>Engravings</b></label>
+               <input type="text" placeholder="Enter Engravings" name="engravings" required />
+
+               <button type="submit">Add New Character</button>
+            </div>
+         </form>
          
       </div>
       
