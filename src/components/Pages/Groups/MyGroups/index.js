a4f879f9121225feaf94d14ea12e6e8dd6c56114
Chuan Wang <chuan.wang101@gmail.com>
2022-06-06 15:26:01 -0700

    my groups

--- src/components/Pages/Groups/MyGroups/index.js
+++ src/components/Pages/Groups/MyGroups/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Navigate, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import './style.css';
 
@@ -22,14 +23,47 @@ function MyGroups({ user }) {
         getMyChars();
     }, []);
 
-    console.log(myChars)
+    let navigate = useNavigate();
+
+    const handleGroupClick = (e) => {
+        // navigate('/');
+        let id;
+        let temp = e.target;
+        while (!id) {
+            id = temp.id;
+            temp = temp.parentElement;
+        }
+        navigate(`/group/${id}`)
+    }
 
     return (
 
-        <div className="page">
-
+        <div className="darkContainer">
             <h1>My Groups</h1>
+            {myChars.map(char => {
+                return (
+                    <div key={char.id}>
+                        <h2>{char.char_name}'s Groups</h2>
+                        {char?.joined?.map(group => {
+                            return (
 
+                                <div key={group.id} id={group.id} className={
+                                    group.id % 4 === 0 ? 'groupPreview bgTree' :
+                                        group.id % 3 === 0 ? 'groupPreview bgGiant' :
+                                            group.id % 2 === 0 ? 'groupPreview bgCity' : 'groupPreview bgSky'
+                                } onClick={handleGroupClick}>
+
+                                    <div>
+                                        <h2>{group.group_name}</h2>
+                                        <p>{group.description}</p>
+                                    </div>
+
+                                </div>
+                            );
+                        })}
+                    </div>
+                )
+            })}
         </div>
 
     );
