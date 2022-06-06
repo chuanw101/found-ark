449f73533d28411dc024814a50c7dff62f38aa94
Chuan Wang <chuan.wang101@gmail.com>
2022-06-06 11:56:46 -0700

    move search

--- src/components/Pages/Groups/AllGroups/index.js
+++ src/components/Pages/Groups/AllGroups/index.js
@@ -7,8 +7,6 @@ import './style.css';
 function AllGroups({ user }) {
 
     const [allGroups, setAllGroups] = useState([]);
-    const [newTag, setTag] = useState("");
-    const [tags, setAllTags] = useState([]);
     // define token
     const token = localStorage.getItem('foundArkJwt');
 
@@ -42,39 +40,6 @@ function AllGroups({ user }) {
         getAllGroups();
     }, []);
 
-    // handle input change
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        if (name === 'tag') {
-            setTag(value);
-        };
-    };
-
-    // validate tag input
-    const tagReg = /^[+-_a-zA-Z0-9]{2,}$/;
-    // add tag to tags array
-    const addTag = () => {
-        if (newTag === '') {
-            return;
-        } else if (tags.includes(newTag)) {
-            return;
-        } else if (!tagReg.test(newTag)) {
-            console.log("invalid tag entry")
-            return;
-        } else {
-            setAllTags([...tags, {tag_name:newTag, active:true}]);
-            setTag('');
-        };
-    };
-
-    // listen for enter key press when adding tags
-    const handleKeyDown = (e) => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            addTag();
-        };
-    };
-
     let navigate = useNavigate();
 
     const handleGroupClick = (e) => {
@@ -101,31 +66,9 @@ function AllGroups({ user }) {
         };
     }
 
-    const handleTagClick = (e) => {
-        if (e.target.className === "savedTagsActive") {
-            //e.target.className = "savedTagsInactive";
-            let tempTags = [...tags];
-            tempTags[e.target.getAttribute('value')].active = false;
-            setAllTags([...tempTags]);
-        } else if (e.target.className === "savedTagsInactive") {
-            //e.target.className = "savedTagsActive";
-            let tempTags = [...tags];
-            tempTags[e.target.getAttribute('value')].active = true;
-            setAllTags([...tempTags]);
-        }
-    }
-
     return (
 
         <div className="darkContainer">
-            <input type="search" id="tag" placeholder="Search tags..." name="tag" value={newTag} onChange={handleInputChange} onKeyDown={handleKeyDown}></input>
-            <p className={newTag === "" || newTag === [] || tagReg.test(newTag) ? 'hidden' : 'visible'}>Tags can only include letters, numbers, and these special characters: + - _</p>
-
-            <div className="savedTags">
-                {tags.map((tag, index) =>
-                    <p className={tag.active?("savedTagsActive"):("savedTagsInactive")}key={index} value={index} onClick={handleTagClick}>{tag.tag_name}<span>❌</span></p>)}
-            </div>
-
             <h1>All Groups</h1>
 
             {allGroups.map((group) => {
