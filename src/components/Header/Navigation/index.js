9846f26181e408bea71c94fd379ac3c7da66012f
Chuan Wang <chuan.wang101@gmail.com>
2022-06-07 16:54:42 -0700

    socket test

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Notifications from './Notifications';
 import './style.css';
+import io from "socket.io-client";
+
+
+const socket = io.connect("http://localhost:3001");
+
 
 function Navigation({ user, logout }) {
+
+    const joinRoom = () => {
+        if (user) {
+            console.log("joiningRoom")
+            socket.emit("setup", user.id);
+        }
+    };
+
+    // useEffect(() => {
+        
+    // }, [user])
+
+    console.log(user)
+
+    useEffect(() => {
+        console.log(user)
+        if(user) {
+            socket.emit("setup", user.id);
+            console.log("Connecting to" + user.id)
+        }
+        socket.on("connected", () => console.log("connected"));
+    }, [user]);
+
+    // useEffect(() => {
+    //     console.log(user)
+    //     if(user) {
+    //         socket.emit("setup", user.id);
+    //         console.log("User Connecting to" + user.id)
+    //     }
+    //     socket.on("connected", () => console.log("connected"));
+    // }, [user]);
+    
+    socket.on("message recieved", (data) => {
+        console.log("noti recieved" + data)
+    });
+
+    
+
+    const test = () => {
+        console.log("hi")
+        const message = "hellllllloooo"
+        socket.emit("new notification", { message, receiver:user.id });
+    }
+    console.log(socket)
     return (
 
         <ul className="navigation">
@@ -16,7 +65,7 @@ function Navigation({ user, logout }) {
                 <>
                     <li className="navItem">
                         <div className="notificationContainer">
-                            <img src="/assets/icons/notification-bell.png" alt="notifications" className="notificationIcon"></img>
+                            <img onClick={test} src="/assets/icons/notification-bell.png" alt="notifications" className="notificationIcon"></img>
                             <span className="notificationAlert"></span>
                         </div>
                         <Notifications />
